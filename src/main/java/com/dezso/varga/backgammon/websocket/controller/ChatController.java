456e06270d9f6392de101df95fad63b6bc7afc06
Varga Dezso <dezso.varga.vd@gmail.com>
2018-03-24 14:43:49 +0200

    added real rabbitmq, private websocket messaging

--- src/main/java/com/dezso/varga/backgammon/websocket/controller/ChatController.java
+++ src/main/java/com/dezso/varga/backgammon/websocket/controller/ChatController.java
@@ -4,6 +4,7 @@ package com.dezso.varga.backgammon.websocket.controller;
  * Created by dezso on 27.12.2017.
  */
 import com.dezso.varga.backgammon.websocket.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -16,6 +17,12 @@ public class ChatController {
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/private.chat.{channelId}")
+    @SendTo("/topic/private.chat.{channelId}")
+    public ChatMessage sendPrivateMessage(@DestinationVariable String channelId, @Payload ChatMessage chatMessage) {
         return chatMessage;
     }
 
